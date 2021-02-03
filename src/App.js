222f6d57e01a75772b47b7fc2d2ed7855e051fc8
Andrew <andrew.mk4@yahoo.com>
2021-02-02 23:46:47 -0800

    frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,107 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Route, Link, Switch } from "react-router-dom";
+import Display from "./Display";
+import Form from "./Form";
 
 function App() {
+  //URL VARIABLE
+  const url = "https://icecreammern.netlify.app"
+  // LIST OF ICECREAM
+  const [icecream, setIcecreams] = React.useState([])
+  // EMPTY ICECREAM
+  const emptyIcecream = {
+    name: "",
+    flavor: "",
+    readyToEat: null
+  }
+
+  // STATE TO TRACK ICECREAM TO EDIT
+  const [selectedIcecream, setSelectedIcecream] = React.useState
+  (emptyIcecream);
+  
+  // LIST OF ICECREAMS
+  const getIcecreams = () => {
+    fetch(url + "/icecream")
+    .then(response => response.json())
+    .then(data => {
+      setIcecreams(data)
+    })
+  }
+
+  // FETCH ICECREAM WHEN PAGE LOADS
+  React.useState(() => {
+    getIcecreams()
+  }, [])
+
+  // HANDLECREATE FUNCTION FOR CREATING ICECREAM
+  const handleCreate = (newIcecream) => {
+    fetch(url + "/icecream", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newIcecream)
+    })
+    .then(() => {
+      getIcecreams()
+    })
+  }
+
+  const selectIcecream = (icecream) => {
+    setSelectedIcecream(icecream)
+  }
+
+  // FUNCTION FOR WHEN ICECREAM IS UPDATED
+  const handleUpdate = (icecream) => {
+    fetch(url + "/icecream/" + icecream._id, {
+      method: "put",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(icecream)
+    })
+    .then(() => {
+      getIcecreams()
+    })
+  }
+
+  // DELETE A ICECREAM
+  const deleteIcecream = (icecream) => {
+    fetch(url + "/icecream/" + icecream._id, {
+      method: "delete"
+    })
+    .then(() => {
+      getIcecreams()
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Ice Cream Listings!</h1>
+      <Link to="/create">
+      <button className="btn1">Create a Ice Cream</button>
+      </Link>
+      <hr />
+      <main>
+        <Switch>
+          <Route exact path="/" render={(rp) => <Display {...rp} icecreams={icecreams} selectIcecream={selectIcecream} deleteIcecream={deleteIcecream}/>} />
+          <Route
+            exact
+            path="/create"
+            render={(rp) => (
+              <Form {...rp} label="create" icecream={emptyIcecream} handleSubmit={handleCreate} />
+            )}
+          />
+          <Route
+            exact
+            path="/edit"
+            render={(rp) => (
+              <Form {...rp} label="update" icecream={selectedIcecream} handleSubmit={handleUpdate} />
+            )}
+          />
+        </Switch>
+      </main>
     </div>
   );
 }
